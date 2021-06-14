674f13e8015d733e663d1219f025e4b8866b82f0
jfontgd <ponponqt@gmail.com>
2021-06-14 21:03:04 +0800

    completed css and js

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
-const form = document.querySelector(".form1");
+const form = document.querySelector('.form');
+const formControl = document.querySelector('.form-control');
 
 
 form.addEventListener('submit', e =>{
     e.preventDefault();
 
-    var name = form.name.value;
-    var lastName = form.lastname.value;
-    var email = form.email.value;
-    var password = form.password.value;
-    const userInfo = [
-        {name, name.length},
-        
-    ];
+    const first= form.fname.value;
+    const last= form.lname.value;
+    const email= form.email.value;
+    const password= form.password.value;
+    const small = formControl.querySelector('small');
+    const icon = formControl.querySelector('.error-icon');
 
-    const filtered = userInfo.filter(field => field.length === 0);
-    console.log(filtered);
 });
-
+   
+        small.style.opacity = "1";
+        small.style.fontStyle = "italic";
+        formControl.classList.add('error');
+        icon.style.opacity = "1";
